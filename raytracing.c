5d839deaf98f2b8e6e9f7698acb11c4103494c16
Marie Diez <spel7900@gmail.com>
2021-02-08 11:23:50 +0100

    add shadow + remove distortion

--- raytracing.c
+++ raytracing.c
@@ -4,15 +4,17 @@
 #include <stdbool.h>
 
 // faire fichier avec leurs fonctions
-
-int N=2;
-int N_L=2;
-
 /* STRUCTURE */
+int N=3;
+int N_L=2;
+int level = 10;
+
+// color
 typedef struct _Couleur {
-  int r, g, b;   
+  int r, g, b;   /* rouge, vert, bleu */
 } Couleur;
 
+// Vecteurs
 typedef struct _Vector2{
 	float x;
 	float y;
@@ -25,8 +27,10 @@ typedef struct _Vector3{
 } Vector3;
 
 typedef struct _Lumiere{
+	// vect directeur
     Vector3 pos;
     int intensity;
+    //light amount=light color∗light intensity.
 } Lumiere;
 
 // image
@@ -41,6 +45,7 @@ typedef struct _Material_para{
     float ks;
 } Material_para;
 
+//scene
 typedef struct _Sphere{
     Material_para para;
     Vector3 pos;
@@ -49,12 +54,12 @@ typedef struct _Sphere{
 } Sphere;
 
 typedef struct _Camera{
+	// vect directeur
     float f;
     Vector3 c;
     Vector3 p;
     Vector3 up;
 } Camera;
-
 typedef struct _Scene{
 	Sphere * list_obj;
     Lumiere * list_lum;
@@ -62,11 +67,14 @@ typedef struct _Scene{
 } Scene;
 
 typedef struct _Rayon{
+	// vect directeur
     Vector3 u;
     Vector3  point;
 } Rayon;
 
-/* FONCTIONS */
+
+Couleur spec_apport(Vector3 pts_espace, Vector3 normal, Lumiere lum, Sphere obj, Vector3 ray, Scene scene);
+
 float norme(Vector3 vect){
     float norm;
     float n=0;
@@ -114,20 +122,23 @@ int save_image_as_ppm(Image I, const char* fichier){
 	return 0;
 }
 
-Rayon pixelToRay(int pixelX, int pixelY, Camera camera){
-    /*float scale = tan(90/180.0);
-    float x= (2.0 * pixelX - 1.0);
+Rayon pixelToRay(float pixelX, float pixelY, Camera camera){
+    /*float x= (2.0 * pixelX - 1.0);
     float y= (2.0 * pixelY - 1.0);
     x *= 1 * scale;
     y *= scale;*/
+
     Rayon rayon;
     rayon.point = camera.c;
     Vector3 pixelCenter;
-    float ordo = 1;
-    float abs = 1;
-    pixelCenter.x = pixelX - camera.p.x;
-    pixelCenter.y = pixelY - camera.p.y;
-    pixelCenter.z = - camera.f;
+    float right = 1;
+    float up = -1;
+    pixelCenter.x = pixelX*up;
+    pixelCenter.y = pixelY ;
+    pixelCenter.z = -1.7;
+    /*pixelCenter.x = pixelX;
+    pixelCenter.y = pixelY;
+    pixelCenter.z = -camera.f;*/
     rayon.u = normalize(pixelCenter);
     return rayon;
 }
@@ -139,6 +150,8 @@ float dot(Vector3 v, Vector3 u) {
     result += v.z*u.z;
     return result;
 }
+
+
 
 bool intersect_obj(Sphere obj, Rayon ray, float * t1, float * t2, Vector3 * normal, Vector3 * P){
     Vector3 o_minus_c;
@@ -168,7 +181,6 @@ bool intersect_obj(Sphere obj, Rayon ray, float * t1, float * t2, Vector3 * norm
         return 1;
     }
 }
-
 bool intersect_obj_bool(Sphere obj, Rayon ray, int l){
     Vector3 o_minus_c;
     o_minus_c.x = ray.point.x + obj.pos.x;
@@ -183,38 +195,6 @@ bool intersect_obj_bool(Sphere obj, Rayon ray, int l){
         return 1;
     }
 }
-
-Couleur diffuse_apport(Vector3 pts_espace, Vector3 normal, Lumiere lum, Sphere obj){
-    Couleur c;
-    Vector3 lumv1;
-    lumv1.x = -lum.pos.x;
-    lumv1.y = -lum.pos.y;
-    lumv1.z = -lum.pos.z;
-
-    c.r = obj.para.kd * obj.c.r * dot(normal,light_direction(pts_espace, lumv1)) * lum.intensity;
-    c.g = obj.para.kd * obj.c.g * dot(normal,light_direction(pts_espace, lumv1)) * lum.intensity;
-    c.b = obj.para.kd * obj.c.b * dot(normal,light_direction(pts_espace, lumv1)) * lum.intensity;
-    return c;
-}
-
-Vector3 reflected_ray(Vector3 normal, Vector3 incident){
-    Vector3 reflected;
-    float c1 = dot( normal, incident );
-    reflected.x = incident.x + (2 * incident.x * c1 );
-    reflected.y = incident.y + (2 * incident.y * c1 );
-    reflected.z = incident.z + (2 * incident.z * c1 );
-    return reflected;
-}
-
-Couleur spec_apport(Vector3 pts_espace, Vector3 normal, Lumiere lum, Sphere obj, Vector3 ray){
-    Couleur c;
-    Vector3 S = reflected_ray(normal, ray);
-    c.r = obj.para.ks * pow(dot(S,light_direction(pts_espace, lum.pos)),3) * lum.intensity;
-    c.g = obj.para.kd * pow(dot(S,light_direction(pts_espace, lum.pos)),3)* lum.intensity;
-    c.b = obj.para.kd * pow(dot(S,light_direction(pts_espace, lum.pos)),3) * lum.intensity;
-    return c;
-}
-
 int visible(Lumiere lum, Vector3 pts_espace, Scene scene, int l){
     Vector3 lum_pts_dir = light_direction(pts_espace, lum.pos);
     Rayon ray;
@@ -230,40 +210,122 @@ int visible(Lumiere lum, Vector3 pts_espace, Scene scene, int l){
     }
     return 1;
 }
+Couleur diffuse_apport(Vector3 pts_espace, Vector3 normal, Lumiere lum, Sphere obj){
+ // iD = KD*c*(n.l) * iLI
+    Couleur c;
+    Vector3 lumv1;
+    lumv1.x = lum.pos.x;
+    lumv1.y = lum.pos.y;
+    lumv1.z = lum.pos.z;
 
-float deg2rad(float deg) { 
-    return deg * M_PI / 180; 
-} 
+    c.r = obj.para.kd * obj.c.r * dot(normal,light_direction(pts_espace, lumv1)) * lum.intensity;
+    c.g = obj.para.kd * obj.c.g * dot(normal,light_direction(pts_espace, lumv1)) * lum.intensity;
+    c.b = obj.para.kd * obj.c.b * dot(normal,light_direction(pts_espace, lumv1)) * lum.intensity;
+    return c;
+}
+
+Vector3 reflected_ray(Vector3 normal, Vector3 incident){
+    /*double cosI = -dot(normal, incident);
+    double sinT2 = (1 - cosI*cosI);
+    double cosT = sqrt(1-sinT2);*/
+    Vector3 reflected;
+  /*  reflected.x = incident.x + (cosI-cosT) * normal.x;
+    reflected.y = incident.y + (cosI-cosT) * normal.y;
+    reflected.z = incident.z + (cosI-cosT) * normal.z;
+*/
+    float c1 = dot( normal, incident );
+    reflected.x = incident.x + (2 * incident.x * c1 );
+    reflected.y = incident.y + (2 * incident.y * c1 );
+    reflected.z = incident.z + (2 * incident.z * c1 );
+    return reflected;
+}
+
+void reflexion_rec(Rayon ray, float *r, float *g, float *b, Scene scene){
+    for(int k=0;k<N;k++){
+        Sphere obj = scene.list_obj[k];
+        float t1, t2;
+        Vector3 normal;
+        Vector3 pts_espace;
+        if(intersect_obj(obj, ray, &t1, &t2, &normal, &pts_espace)){
+            Couleur diffuse;
+            diffuse.r=0;
+            diffuse.g=0;
+            diffuse.b=0;
+            for(int l=0;l<N_L;l++){ 
+                if (visible(scene.list_lum[l],pts_espace, scene, k)){
+                    Couleur res = diffuse_apport(pts_espace, normal, scene.list_lum[l], obj);
+                    Couleur res2 = spec_apport(pts_espace, normal, scene.list_lum[l], obj, ray.u,scene);
+                    *r += (res.r);
+                    *g += (res.g);
+                    *b += (res.b);
+                }
+            }
+        } else {
+            *r = 0;
+            *g = 0;
+            *b = 0;
+        }
+    } 
+}
+
+Couleur spec_apport(Vector3 pts_espace, Vector3 normal, Lumiere lum, Sphere obj, Vector3 ray, Scene scene){
+    //id = ks * (S.l)^ns*I 
+    Couleur c;
+    Vector3 S = reflected_ray(normal, ray);
+    float r = 0;
+    float g = 0;
+    float b = 0;
+    Rayon ray1;
+    ray1.u = S;
+    ray1.point = pts_espace;
+    if  (level  > 0){
+        level-=1;
+        reflexion_rec(ray1,&r,&g,&b,scene);
+    }
+    c.r = obj.para.ks * pow(dot(S,light_direction(pts_espace, lum.pos)),5) * lum.intensity +r;
+    c.g = obj.para.ks * pow(dot(S,light_direction(pts_espace, lum.pos)),5)* lum.intensity+g ;
+    c.b = obj.para.ks * pow(dot(S,light_direction(pts_espace, lum.pos)),5) * lum.intensity+b;
+    return c;
+}
+
+
+float deg2rad(float deg) 
+{ return deg * M_PI / 180; } 
+
+
 
 void imageRender(Scene scene){
     Image img;
     img.width = 500;
     img.height = 500;
-    float fov = deg2rad(90);
 
     img.pixels = malloc(img.height * (img.width * sizeof(Couleur)));
     for (int i=0; i < img.width; i++){
             img.pixels[i] = malloc(img.height * sizeof(Couleur));
     }
     Couleur noir;
-    noir.r = 0;
-    noir.g = 0;
+    noir.r = 255;
+    noir.g = 255;
     noir.b = 0;
-    for(int k=0;k<N;k++){
-        Sphere obj = scene.list_obj[k];
-        for(int i=0; i < img.width; i++){
-            for(int j=0; j < img.height; j++){
-                // Convertir les cordonnées de l'image dans l'espace...
-                
-                //float imageAspectRatio = img.width / (float)img.height; 
-                //float Px = (2 * ((i + 0.5) / img.width) - 1) * tan(fov / 2 * M_PI / 180) * imageAspectRatio; 
-                //float Py = (1 - 2 * ((j+ 0.5) / img.height)) * tan(fov / 2 * M_PI / 180); 
-                // Rayon ray = pixelToRay(Px, Py, scene.camera);
-
-                // centrer image
-                float x= i-img.width/2;
-                float y= j-img.height/2;
-                Rayon ray = pixelToRay(x, y, scene.camera);
+    for(int i=0; i < img.width; i++){
+        for(int j=0; j < img.height; j++){
+            // Convertir les cordonnées de l'image dans l'espace...
+            float fov = 90;
+            float imageAspectRatio = img.width / (float)img.height; 
+            float Px = (2 * ((i + 0.5) / img.width) - 1) * tan(fov / 2 * M_PI / 180) * imageAspectRatio; 
+            float Py = (1 - 2 * ((j+ 0.5) / img.height)) * tan(fov / 2 * M_PI / 180); 
+            Rayon ray = pixelToRay(Px, Py, scene.camera);
+            /*float r = 0;
+            float g = 0;
+            float b = 0;
+            reflexion_rec(ray, &r, &g, &b, scene);
+            Couleur col;
+            col.r=r;
+            col.g=g;
+            col.b=b;
+            img.pixels[i][j] = col;*/
+            for(int k=0;k<N;k++){
+                Sphere obj = scene.list_obj[k];
                 float t1, t2;
                 Vector3 normal;
                 Vector3 pts_espace;
@@ -275,15 +337,18 @@ void imageRender(Scene scene){
                     for(int l=0;l<N_L;l++){ 
                         if (visible(scene.list_lum[l],pts_espace, scene, k)){
                             Couleur res = diffuse_apport(pts_espace, normal, scene.list_lum[l], obj);
-                            Couleur res2 = spec_apport(pts_espace, normal, scene.list_lum[l], obj, ray.u);
-                            diffuse.r += (res.r + res2.r);
-                            diffuse.g += (res.g + res2.g);
-                            diffuse.b += (res.b + res2.b);
+                            Couleur res2 = spec_apport(pts_espace, normal, scene.list_lum[l], obj, ray.u, scene);
+                            diffuse.r += (res.r+res2.r);
+                            diffuse.g += (res.g+res2.g);
+                            diffuse.b += (res.b+res2.b);
                             img.pixels[i][j] = diffuse;
                         }
                     }
                 } else{
                         if (img.pixels[i][j].r == 0 && img.pixels[i][j].g == 0 && img.pixels[i][j].b == 0){
+                            noir.r = (ray.u.y+1)*255;
+                            noir.g = (ray.u.y+1)*255;
+                            noir.b = (ray.u.y+1)*0;
                             img.pixels[i][j] = noir;
                         }
                     }
@@ -294,9 +359,7 @@ void imageRender(Scene scene){
 }
 
 int main(){
-
     Scene scene;
-
     Camera  camera;
     camera.f = 70;
     Vector3 pos_camera;
@@ -305,67 +368,84 @@ int main(){
     pos_camera.z = 0;
     camera.c = pos_camera;
 
-
-    Sphere sphere1;
     Couleur c1;
     c1.r = 255;
     c1.g = 0;
     c1.b = 0;
+    Sphere sphere1;
     Vector3 sphere_pos;
     sphere_pos.x = 0;
     sphere_pos.y = 0;
-    sphere_pos.z = 300;
+    sphere_pos.z = 20;
     Material_para para;
-    para.ks = 0.2;
+    para.ks = 0.1;
     para.kd = 0.2;
     sphere1.para = para;
     sphere1.c = c1;
     sphere1.pos = sphere_pos;
-    sphere1.r = 250;
+    sphere1.r = 7;
 
-    Sphere sphere2;
     Couleur c2;
     c2.r = 0;
     c2.g = 255;
     c2.b = 0;
+
+    Sphere sphere2;
     Vector3 sphere_pos2;
-    sphere_pos2.x = 500;
-    sphere_pos2.y = 200;
-    sphere_pos2.z = 300;
+    sphere_pos2.x = -5;
+    sphere_pos2.y =  -5;
+    sphere_pos2.z = 12;
     Material_para para2;
     para2.ks = 0.2;
-    para2.kd = 0.2;
+    para2.kd = 0.06;
     sphere2.para = para2;
     sphere2.c = c2;
     sphere2.pos = sphere_pos2;
-    sphere2.r = 150;
+    sphere2.r = 3;
+
+    Couleur c3;
+    c3.r = 0;
+    c3.g = 0;
+    c3.b = 255;
+    Sphere sol;
+    Vector3 sol_pos;
+    sol_pos.x = 90;
+    sol_pos.y =  0;
+    sol_pos.z = 70;
+    Material_para para3;
+    para3.ks = 0.15;
+    para3.kd = 0.06;
+    sol.para = para3;
+    sol.c = c3;
+    sol.pos = sol_pos;
+    sol.r = 70;
 
     scene.camera = camera;
     scene.list_obj = malloc(N* sizeof(Sphere));
-    scene.list_obj[0] = sphere1;
-    scene.list_obj[1] = sphere2;
+    scene.list_obj[0] = sol;
+    scene.list_obj[1] = sphere1;
+    scene.list_obj[2] = sphere2;
 
     Lumiere lum;
     Vector3 position_lum;
-    position_lum.x = 100;
-    position_lum.y = 100;
+    position_lum.x = -30;
+    position_lum.y = -60;
     position_lum.z = 0;
     lum.pos = position_lum;
-    lum.intensity = 7;
-
+    lum.intensity = 20;
     Lumiere lum2;
     Vector3 position_lum2;
-    position_lum2.x = -100;
-    position_lum2.y = -100;
+    position_lum2.x = -60;
+    position_lum2.y = 100;
     position_lum2.z = 0;
     lum2.pos = position_lum2;
-    lum2.intensity = 1;
-
+    lum2.intensity = 5;
     scene.list_lum = malloc(N_L* sizeof(Lumiere));
     scene.list_lum[0] = lum;
     scene.list_lum[1] = lum2;
 
     imageRender(scene);
-
+    
+   
     return EXIT_SUCCESS;
 }
